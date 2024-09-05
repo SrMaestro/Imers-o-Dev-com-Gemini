479f5ec1b0b416ab190f9570f14f56bdf0acf964
Luis Felipe R . L <luisramires29032002@gmail.com>
2024-09-05 18:55:26 -0300

    Aula 04

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-console.log('ola,mundo')
+// console.log(filmesGhibli);
+
+
+function pesquisar (){
+
+    let section = document.getElementById('resultados-pesquisa');
+
+    let resultado = ""
+    
+    for(let dado of  filmesGhibli){
+        resultado += `
+        <div class="item-resultado">
+              <h2>
+                <a
+                  href="#"
+                  target="_blank"
+                  >${dado.titulo}
+                </a>
+              </h2>
+              <p class="descricao-meta">
+                ${dado.descricao}
+              </p>
+              <a
+                href="https://www.minecraft.net/pt-br"
+                target="_blank"
+                >click aqui</a
+              >
+              
+            </div>
+    
+    `;
+    
+    }
+    
+    
+    section.innerHTML = resultado;
+    
+    
+}
+
+
+
